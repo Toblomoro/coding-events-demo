b733780b22799c834cb8b217bdec3a9096625bfe
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-11-02 16:51:32 -0500

    Had to fiddle around to get workign again

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,15 +1,22 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
 @Entity
-public class Event extends AbstractEntity {
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -65,9 +72,25 @@ public class Event extends AbstractEntity {
         this.type = type;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
