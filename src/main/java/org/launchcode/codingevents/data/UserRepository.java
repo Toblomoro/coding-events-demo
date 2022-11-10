5cddd5027c5760c7dd044a5d8f6c76f25066d255
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-11-10 16:28:38 -0600

    DTO beign funny

--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -1,4 +1,9 @@
 package org.launchcode.codingevents.data;
 
-public interface UserRepository {
+import org.launchcode.codingevents.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByUsername (String username);
+
 }
